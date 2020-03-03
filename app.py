407f336bb56e4d4820ddd2193cd7c3a9441d6be8
Eldar Garayev <egaraev@gmail.com>
2020-03-03 16:45:29 -0500

    only GET left

--- app.py
+++ app.py
@@ -1,90 +1,187 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from urllib.parse import parse_qs
+from bson.json_util import dumps
+from flask import Flask, request, jsonify
+import json
+import ast
+from importlib.machinery import SourceFileLoader
 from pymongo import MongoClient
 
+DATABASE = MongoClient()['restfulapi'] # DB_NAME
+DEBUG = True
+client = MongoClient('localhost', 27017)
+
+
+
+def parse_query_params(query_string):
+    """
+        Function to parse the query parameter string.
+        """
+    # Parse the query param string
+    query_params = dict(parse_qs(query_string))
+    # Get the value from the list
+    query_params = {k: v[0] for k, v in query_params.items()}
+    return query_params
+	
+	
 app = Flask(__name__)
-api = Api(app)
 
 
-client = MongoClient('mongodb://localhost:27017')
+# Select the database
+db = client.restfulapi
+# Select the collection
+collection = db.users
 
-mydb = client['test_database']
-
-myrecord = [
-    {
-        "name": "Nicholas",
-        "age": 42,
-        "occupation": "Network Engineer"
-    },
-    {
-        "name": "Elvin",
-        "age": 32,
-        "occupation": "Doctor"
-    },
-    {
-        "name": "Jass",
-        "age": 22,
-        "occupation": "Web Developer"
+@app.route("/")
+def get_initial_response():
+    """Welcome message for the API."""
+    # Message to the user
+    message = {
+        'apiVersion': 'v1.0',
+        'status': '200',
+        'message': 'Welcome to the Flask API'
     }
-]
+    # Making the message looks good
+    resp = jsonify(message)
+    # Returning the object
+    return resp
 
 
-record_id = mydb.mytable.insert(myrecord)
+@app.route("/api/v1/users", methods=['POST'])
+def create_user():
+    """
+       Function to create new users.
+       """
+    try:
+        # Create new users
+        try:
+            body = ast.literal_eval(json.dumps(request.get_json()))
+        except:
+            # Bad request as request body is not available
+            # Add message for debugging purpose
+            return "", 400
 
-users = mydb.mytable.find( {} )
+        record_created = collection.insert(body)
+
+        # Prepare the response
+        if isinstance(record_created, list):
+            # Return list of Id of the newly created item
+            return jsonify([str(v) for v in record_created]), 201
+        else:
+            # Return Id of the newly created item
+            return jsonify(str(record_created)), 201
+    except:
+        # Error while trying to create the resource
+        # Add message for debugging purpose
+        return "", 500
 
 
+@app.route("/api/v1/users", methods=['GET'])
+def fetch_users():
+    """
+       Function to fetch the users.
+       """
+    try:
+        # Call the function to get the query params
+        query_params = parse_query_params(request.query_string)
+        # Check if dictionary is not empty
+        if query_params:
 
-class User(Resource):
-    def get(self, name):
-        for user in users:
-            if(name == user["name"]):
-                return user, 200
-        return "User not found", 404
+            # Try to convert the value to int
+            query = {k: int(v) if isinstance(v, str) and v.isdigit() else v for k, v in query_params.items()}
 
-    def post(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
+            # Fetch all the record(s)
+            records_fetched = collection.find(query)
 
-        for user in users:
-            if(name == user["name"]):
-                return "User with name {} already exists".format(name), 400
+            # Check if the records are found
+            if records_fetched.count() > 0:
+                # Prepare the response
+                return dumps(records_fetched)
+            else:
+                # No records are found
+                return "", 404
 
-        user = {
-            "name": name,
-            "age": args["age"],
-            "occupation": args["occupation"]
-        }
-        users.append(user)
-        return user, 201
+        # If dictionary is empty
+        else:
+            # Return all the records as query string parameters are not available
+            if collection.find().count > 0:
+                # Prepare response if the users are found
+                return dumps(collection.find())
+            else:
+                # Return empty array if no users are found
+                return jsonify([])
+    except:
+        # Error while trying to fetch the resource
+        # Add message for debugging purpose
+        return "", 500
 
-    def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("age")
-        parser.add_argument("occupation")
-        args = parser.parse_args()
 
-        for user in users:
-            if(name == user["name"]):
-                user["age"] = args["age"]
-                user["occupation"] = args["occupation"]
-                return user, 200
-        
-        user = {
-            "name": name,
-            "age": args["age"],
-            "occupation": args["occupation"]
-        }
-        users.append(user)
-        return user, 201
+@app.route("/api/v1/users/<user_id>", methods=['POST'])
+def update_user(user_id):
+    """
+       Function to update the user.
+       """
+    try:
+        # Get the value which needs to be updated
+        try:
+            body = ast.literal_eval(json.dumps(request.get_json()))
+        except:
+            # Bad request as the request body is not available
+            # Add message for debugging purpose
+            return "", 400
 
-    def delete(self, name):
-        global users
-        users = [user for user in users if user["name"] != name]
-        return "{} is deleted.".format(name), 200
-      
-api.add_resource(User, "/user/<string:name>")
+        # Updating the user
+        records_updated = collection.update_one({"id": int(user_id)}, body)
 
+        # Check if resource is updated
+        if records_updated.modified_count > 0:
+            # Prepare the response as resource is updated successfully
+            return "", 200
+        else:
+            # Bad request as the resource is not available to update
+            # Add message for debugging purpose
+            return "", 404
+    except:
+        # Error while trying to update the resource
+        # Add message for debugging purpose
+        return "", 500
+
+
+@app.route("/api/v1/users/<user_id>", methods=['DELETE'])
+def remove_user(user_id):
+    """
+       Function to remove the user.
+       """
+    try:
+        # Delete the user
+        delete_user = collection.delete_one({"id": int(user_id)})
+
+        if delete_user.deleted_count > 0 :
+            # Prepare the response
+            return "", 204
+        else:
+            # Resource Not found
+            return "", 404
+    except:
+        # Error while trying to delete the resource
+        # Add message for debugging purpose
+        return "", 500
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Send message to the user with notFound 404 status."""
+    # Message to the user
+    message = {
+        "err":
+            {
+                "msg": "This route is currently not supported. Please refer API documentation."
+            }
+    }
+    # Making the message looks good
+    resp = jsonify(message)
+    # Sending OK response
+    resp.status_code = 404
+    # Returning the object
+    return resp
+	
 app.run(debug=True)
